0d06d12e52d4baef48bb5dc2479bea28870f5bc5
mathew-koshy <mathew.koshy@sedai.io>
2024-12-18 13:16:27 -0500

    clean up

--- examples/resources/monitoring_providers_creation_example/resource.tf
+++ examples/resources/monitoring_providers_creation_example/resource.tf
@@ -15,7 +15,7 @@ resource "sedai_create_gke_monitoring_provider" "gke_monitoring_provider" {
   account_id = sedai_create_account.gke_account.id
   service_account_json = "service_account_json_content"
   project_id = "project_id"
-  integration_type = "AGENTLESS"
+  integration_type = sedai_create_account.gke_account.integration_type
 }
 
 # EKS account for monitoring providers
@@ -29,7 +29,7 @@ resource "sedai_create_account" "eks_account" {
 # Federated prometheus monitoring provider with JWT
 resource "sedai_create_federated_prometheus_monitoring_provider" "federate_prometheus_monitoring_provider_jwt" {
     account_id = sedai_create_account.eks_account.id
-    integration_type = "AGENTLESS/AGENT_BASED"
+    integration_type = sedai_create_account.eks_account.integration_type
     endpoint = "endpoint"
     bearer_token = "bearer_token"
 }
@@ -37,8 +37,7 @@ resource "sedai_create_federated_prometheus_monitoring_provider" "federate_prome
 # Federated prometheus monitoring provider with client credentials
 resource "sedai_create_federated_prometheus_monitoring_provider" "federate_prometheus_monitoring_provider_client_creds" {
     account_id = sedai_create_account.eks_account.id
-    integration_type = "AGENTLESS/AGENT_BASED"
-    endpoint = "endpoint"
+    integration_type = sedai_create_account.eks_account.integration_type
     token_endpoint = "token_endpoint"
     client_id = "client_id"
     client_secret = "client_secret"
@@ -47,14 +46,14 @@ resource "sedai_create_federated_prometheus_monitoring_provider" "federate_prome
 # Federated prometheus monitoring provider with no auth
 resource "sedai_create_federated_prometheus_monitoring_provider" "federate_prometheus_monitoring_provider_no_auth" {
     account_id = sedai_create_account.eks_account.id
-    integration_type = "AGENTLESS/AGENT_BASED"
+    integration_type = sedai_create_account.eks_account.integration_type
     endpoint = "endpoint"
 }
 
 # Datadog monitoring provider
 resource "sedai_create_datadog_monitoring_provider" "datadog_monitoring_provider" {
     account_id = sedai_create_account.eks_account.id
-    integration_type = "AGENTLESS/AGENT_BASED"
+    integration_type = sedai_create_account.eks_account.integration_type
     api_key = "api_key"
     application_key = "application_key"
 }
@@ -62,7 +61,7 @@ resource "sedai_create_datadog_monitoring_provider" "datadog_monitoring_provider
 # New relic monitoring provider
 resource "sedai_create_newrelic_monitoring_provider" "newrelic_monitoring_provider" {
     account_id = sedai_create_account.eks_account.id
-    integration_type = "AGENTLESS/AGENT_BASED"
+    integration_type = sedai_create_account.eks_account.integration_type
     api_key = "api_key"
     api_server = "api_server"
     newrelic_account_id = "newrelic_account_id"
